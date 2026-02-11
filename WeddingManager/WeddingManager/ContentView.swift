284fd8975f29fd4152dfd6c19d2b0eb3a1af7425
Bartosz <bartosz.jurczyk154@gmail.com>
2026-02-11 08:20:55 +0100

    prePush

---
+++ WeddingManager/WeddingManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeddingManager
+//
+//  Created by Bartosz Jurczyk on 10/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeddingManager/WeddingManager/WeddingManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeddingManagerApp.swift
+//  WeddingManager
+//
+//  Created by Bartosz Jurczyk on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeddingManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
